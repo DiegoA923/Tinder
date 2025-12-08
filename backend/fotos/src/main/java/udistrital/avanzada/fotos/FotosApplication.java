696ee84c8109c659429b110fc792a5855d6b15b9
YMB030 <129144487+YMB030@users.noreply.github.com>
2025-12-08 17:04:44 -0500

    chore crearcion esquelo microservicios fotos

---
+++ backend/fotos/src/main/java/udistrital/avanzada/fotos/FotosApplication.java
@@ -0,0 +1,13 @@
+package udistrital.avanzada.fotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotosApplication.class, args);
+	}
+
+}
