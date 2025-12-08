643d707493eb621032986907d3316d7e5504ef8e
Diego <diego3481009@gmail.com>
2025-12-08 16:09:44 -0500

    Estructura Inicial

---
+++ usuarios/usuarios/src/main/java/udistrital/avanzada/usuarios/UsuariosApplication.java
@@ -0,0 +1,13 @@
+package udistrital.avanzada.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosApplication.class, args);
+	}
+
+}
